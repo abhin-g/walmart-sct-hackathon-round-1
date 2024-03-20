d5a2583f8a930678fb9714d8355264dcfb248d11
abhin-g <100078457+abhin-g@users.noreply.github.com>
2024-03-20 21:31:50 +0530

    Add files via upload

---
+++ code/part_a/run.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+
+input_file = sys.argv[1]
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # radius of Earth in kilometers
+    phi1 = np.radians(lat1)
+    phi2 = np.radians(lat2)
+    delta_phi = np.radians(lat2 - lat1)
+    delta_lambda = np.radians(lon2 - lon1)
+    a = (
+        np.sin(delta_phi / 2) ** 2
+        + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2) ** 2
+    )
+    res = R * (2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a)))
+    return np.round(res, 2)
